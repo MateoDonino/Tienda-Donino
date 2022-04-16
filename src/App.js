3dd3d73e02e7912ef6cf92792bec78fb5f01c190
MateoDonino <96183188+MateoDonino@users.noreply.github.com>
2022-04-16 13:27:38 -0300

    Se agrega el proyecto

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>Hola mundo, ahora con React!</h1>
+    <ul>
+      <li>1</li>
+      <li>{1+1}</li>
+      <li>{2+1}</li>
+      <li>{3+1}</li>
+    </ul>
+    </>
+  );
+}
+
+export default App;
